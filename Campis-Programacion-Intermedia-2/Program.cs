b34c3cf9e6f070618ae324c13ec950c958985e03
Julio Cesar Ambriz Quijada <julio-cesar.ambriz@atos.net>
2022-07-22 17:23:12 -0500

    Finalizada la implementacion de metodos reversos y la del palindromo al programa.

--- Campis-Programacion-Intermedia-2/Program.cs
+++ Campis-Programacion-Intermedia-2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Campis_Programacion_Intermedia_2
 {
@@ -24,14 +26,104 @@ namespace Campis_Programacion_Intermedia_2
             public int Id { get; set; }
             public string Nombre { get; set; }
             public string Pass { get; set; }
-            
+
             public Struct(int id, string nombre, string pass)
             {
-                Id =id;
-                Nombre =nombre;
+                Id = id;
+                Nombre = nombre;
                 Pass = pass;
             }
         }
+        static void ReverseWithQueue(int[] arreglo)
+        {
+            var timer = new Stopwatch();
+
+            timer.Start();
+            Queue<int> cola = new Queue<int>();
+
+            foreach(var item in arreglo)
+            {
+                cola.Enqueue(item);
+            }
+            var arreglo2 = cola.Reverse().ToArray();
+            foreach (var item in arreglo2)
+            {
+                Console.WriteLine(item);
+            }
+            timer.Stop();
+
+            TimeSpan elapsedTime = timer.Elapsed;
+            Console.WriteLine("Tiempo de ejecucion: " + elapsedTime.ToString(@"m\:ss\.fff"));
+            Console.WriteLine("Primer elemento: " + arreglo2[0]);
+        }
+
+        static void ReverseWithDict(int[] arreglo)
+        {
+            var timer = new Stopwatch();
+
+            timer.Start();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for(int i = 0; i < arreglo.Length; i++)
+            {
+                dict.Add(i, arreglo[i]);
+            }
+            var reversedDictionary = dict.Reverse();
+
+            var arreglo2 = reversedDictionary.Select(z => z.Value).ToArray();
+
+            foreach(var item in arreglo2)
+            {
+                Console.WriteLine(item);
+            }
+
+            timer.Stop();
+
+            TimeSpan elapsedTime = timer.Elapsed;
+            Console.WriteLine("Tiempo de ejecucion: " + elapsedTime.ToString(@"m\:ss\.fff"));
+            Console.WriteLine("Primer elemento: " + arreglo2[0]);
+
+        }
+
+        static void ReverseWithLis(int[] arreglo)
+        {
+            var timer = new Stopwatch();
+
+            timer.Start();
+
+            List<int> lista = new List<int>();
+            foreach(var item in arreglo)
+            {
+                lista.Add(item);
+            }
+            List<int> listaInversa = Enumerable.Reverse(lista).ToList();
+            listaInversa.ForEach((item) => Console.WriteLine(item));
+
+            timer.Stop();
+
+            TimeSpan elapsedTime = timer.Elapsed;
+            Console.WriteLine("Tiempo de ejecucion: " + elapsedTime.ToString(@"m\:ss\.fff"));
+            Console.WriteLine("Primer elemento: " + listaInversa[0]);
+        }
+
+        static bool Palindromo(string texto)
+        {
+            Stack<string> Pila = new Stack<string>();
+
+            foreach (char c in texto)
+            {
+                Pila.Push(c.ToString());
+            }
+            string? texto2 = null;
+            while (Pila.Count > 0)
+            {
+                texto2 += Pila.Peek(); Pila.Pop();
+            }
+
+            if (texto.ToLower().Equals(texto2.ToLower())) return true;
+            return false;
+            
+        }
+
         static void Main(string[] args)
         {
             // Tuplas
@@ -88,7 +180,38 @@ namespace Campis_Programacion_Intermedia_2
             Console.WriteLine("Tiempo Ejecucion Struct: " + tiempoEjecucionStruct.ToString(@"m\:ss\.fff"));
 
             // Colas, Listas, Diccionarios
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
 
+            // A queue can be enumerated without disturbing its contents.
+            int[] arreglo = new int[] { 1, 3, 5, 7, 9 };
+            Console.WriteLine("Arreglo origianl: ");
+            foreach(var item in arreglo)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Arreglo inverso con Cola: ");
+            ReverseWithQueue(arreglo);
+            Console.WriteLine();
+            Console.WriteLine("Arreglo inverso con Lista: ");
+            ReverseWithLis(arreglo);
+            Console.WriteLine();
+            Console.WriteLine("Arreglo inverso con Diccionario: ");
+            ReverseWithDict(arreglo);
+            Console.WriteLine();
+
+            // Es Palindromo?
+            string nombre = "Anna";
+            Console.WriteLine(Palindromo(nombre));
+
+            nombre = "Zeppelin";
+            Console.WriteLine(Palindromo(nombre));
 
         }
     }
